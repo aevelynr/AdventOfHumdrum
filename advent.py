a771c25de9cb6ed2f3d5bbe0a8bf153688690ee6
aevelynr <aeroeser@yahoo.com>
2022-11-21 20:30:53 -0600

    download added

--- advent.py
+++ advent.py
@@ -1,5 +1,8 @@
+import os
 import argparse
 import requests
+
+DATA_URL = 'https://adventofcode.com/{}/day/{}/input'
 
 if __name__ == "__main__":
     ADVENT_OF_CODE_YEAR = 2021
@@ -32,7 +35,16 @@ if __name__ == "__main__":
         :param problem:
         :return:
         '''
-        pass
+        cookies = {
+            'session': '53616c7465645f5f132cad5af280798cda1c1bfd5d3df3c4c25cb4a57b3cba00d7338e6dab18b6b158385945bb10cc78e113f60773defb657986ce10bfe6f4eb'}
+        #response = requests.get('https://adventofcode.com/2021/day/1/input', cookies=cookies)
+        response = requests.get(DATA_URL.format(ADVENT_OF_CODE_YEAR, problem), cookies=cookies)
+
+        if response.ok:
+            data_path = os.path.join(os.path.curdir, 'data', f'day_{problem}.txt')
+            if not os.path.isfile(data_path):
+                with open(data_path, 'w+') as data_file:
+                    data_file.write(response.text)
 
 
     def setup(problem):
