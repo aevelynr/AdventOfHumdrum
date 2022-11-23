d9e8fdcc10a23c0613925a63f52e86870d3821aa
R <rbeakes@DESKTOP-U67G7PO.localdomain>
2022-11-22 23:39:32 -0500

    BLACK MAGIC DO NOT TOUCH

--- advent.py
+++ advent.py
@@ -1,11 +1,13 @@
 import os
 import argparse
 import requests
+from utils.cookies import COOKIES
 
+
+ADVENT_OF_CODE_YEAR = 2021
 DATA_URL = 'https://adventofcode.com/{}/day/{}/input'
 
 if __name__ == "__main__":
-    ADVENT_OF_CODE_YEAR = 2021
     parser = argparse.ArgumentParser(
         prog='AdventOfHumdrum',
         description='Advent of Code'
@@ -17,7 +19,6 @@ if __name__ == "__main__":
     parser.add_argument('-s', '--setup', action='store_true', default=False)
 
     args = parser.parse_args()
-
 
     def run(problem):
         '''
@@ -35,16 +36,13 @@ if __name__ == "__main__":
         :param problem:
         :return:
         '''
-        cookies = {
-            'session': '53616c7465645f5f132cad5af280798cda1c1bfd5d3df3c4c25cb4a57b3cba00d7338e6dab18b6b158385945bb10cc78e113f60773defb657986ce10bfe6f4eb'}
-        #response = requests.get('https://adventofcode.com/2021/day/1/input', cookies=cookies)
-        response = requests.get(DATA_URL.format(ADVENT_OF_CODE_YEAR, problem), cookies=cookies)
+        response = requests.get(DATA_URL.format(ADVENT_OF_CODE_YEAR, problem), cookies=COOKIES)
 
         if response.ok:
             data_path = os.path.join(os.path.curdir, 'data', f'day_{problem}.txt')
-            if not os.path.isfile(data_path):
-                with open(data_path, 'w+') as data_file:
-                    data_file.write(response.text)
+            print(response.text)
+            with open(data_path, 'wb+') as data_file:
+                data_file.write(response.content)
 
 
     def setup(problem):
---
+++ utils/cookies.py
@@ -0,0 +1,40 @@
+###################################################################################################
+##################################BLACK MAGIC DO NOT TOUCH#########################################
+###################################################################################################
+##################################BLACK MAGIC DO NOT TOUCH#########################################
+###################################################################################################
+##################################BLACK MAGIC DO NOT TOUCH#########################################
+###################################################################################################
+import os
+import glob
+import platform
+
+from browser_cookie3 import chrome, firefox, safari
+
+
+DOMAIN_NAME = 'adventofcode'
+
+
+def get_cookies():
+    if 'microsoft-standard' in platform.uname().release:
+        WSL_ROOT_DIR = os.path.join('/mnt', 'c',)
+        WINDOWS_CHROME_COOKIES = os.path.join(WSL_ROOT_DIR, 'Users', '*', 'AppData', 'Local', 'Google', 'Chrome', 'User Data', 'Default', 'Cookies')
+        WINDOWS_FIREFOX_COOKIES = os.path.join(WSL_ROOT_DIR, 'Users', '*', 'AppData', 'Roaming', 'Mozilla', 'Firefox', 'Profiles', '*', 'cookies.sqlite')
+
+        for file, cookie_jar in [
+            (WINDOWS_CHROME_COOKIES, chrome),
+            (WINDOWS_FIREFOX_COOKIES, firefox)
+        ]:
+            for cookie_file in glob.glob(file):
+                cookie = cookie_jar(domain_name=DOMAIN_NAME, cookie_file=cookie_file)
+                if cookie:
+                    return cookie
+        raise Exception('Could not find the cookie for WSL')
+
+    for browser_cookies in [chrome, firefox, safari]:
+        cookie = browser_cookies(domain_name='adventofcode')
+        if cookie:
+            return cookie
+
+
+COOKIES = get_cookies()
